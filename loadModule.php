889e3fc472a5f67e83325394c0ad8e2613ed10da
Cris Higham <cris@justblink.co.za>
2023-03-07 11:58:35 +0200

    Modified loadModule
    Removed Content

--- loadModule.php
+++ loadModule.php
@@ -1,4 +1,4 @@
 <?php
 \Tina4\Module::addModule("Login Module", "1.0.0", "login", static function (\Tina4\Config $config) {
-    (new Content())->addConfigMethods($config);
+    //(new Content())->addConfigMethods($config);
 });
