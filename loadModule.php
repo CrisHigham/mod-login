1116f48e1e5217f7c558c641247255951bc36fd5
Cris Higham <cris@justblink.co.za>
2023-05-15 16:17:47 +0200

    Modified loadModule
    changed namespace
    updated composer requirements

--- loadModule.php
+++ loadModule.php
@@ -1,4 +1,4 @@
 <?php
-\Tina4\Module::addModule("Login Module", "1.0.0", "login", static function (\Tina4\Config $config) {
+\Tina4\Module::addModule("Login Module", "1.0.0", "myNamespace", static function (\Tina4\Config $config) {
     //(new Content())->addConfigMethods($config);
 });
